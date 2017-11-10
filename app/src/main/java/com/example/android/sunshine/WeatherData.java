c74716eeb0ef89ec9a6ce576a154e31ec9047dd8
murnat98 <murnat98@yandex.ru>
2017-11-10 20:24:31 +0300

    S02 done

---
+++ app/src/main/java/com/example/android/sunshine/WeatherData.java
@@ -0,0 +1,18 @@
+package com.example.android.sunshine;
+
+public class WeatherData {
+
+    public static String[] getFakeWeatherData() {
+        return new String[]{
+                "weather1",
+                "weather2",
+                "weather3",
+                "weather4",
+                "weather5",
+                "weather6",
+                "weather7",
+                "weather8",
+        };
+    }
+
+}
